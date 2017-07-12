176f24f5ffeed961b78d0d921cb7dd34456f1c8b
aik <aik>
2017-07-12 17:21:52 +0100

    update

--- hello_ml.py
+++ hello_ml.py
@@ -1,7 +1,23 @@
 import numpy as np
 
-class PERCEPTRON()
+class PERCEPTRON:
+
+    def __init__(self, eta=0.01, n_itr=10):
+        pass
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
     
-    def __init__(self, eta=0.01, n_itr=10)
-        self.eta = eta;
-        self.n_itr = n_itr;
+    # fit the training data
+    def fit(self, X, y):
+
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.errors_ = []
+
+        for _ in range(self.n_itr):
+            pass
+
+        pass
